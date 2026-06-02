7560f0186289dae75fec60f68f6edff821345328
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:25:30 +0300

    Update sessions.js

--- 15/src/routes/sessions.js
+++ 15/src/routes/sessions.js
@@ -4,6 +4,28 @@ export default (app) => {
   const users = generateUsers();
 
   // BEGIN (write your solution here)
+  app.get("/sessions/new", (req, res) => {
+    res.view('src/views/sessions/new');
+  });
 
+  app.post("/sessions", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find((u) => u.username === username);
+
+    if (!user || decrypt(user.password) !== password) {
+      res.render("sessions/new", {
+        error: "Wrong username or password"
+      });
+      return res.redirect('/sessions/new');
+    }
+
+    req.session.username = username;
+    res.redirect('/');
+  });
+
+  app.post("/sessions/delete", (req, res) => {
+    req.session.username = null;
+    res.redirect('/');
+  });
   // END
 };
