e12ae6cda1b5081e6d5e0ed8d72ed82b94a9acd7
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:36:35 +0300

    Update server.js

--- 1/src/server.js
+++ 1/src/server.js
@@ -3,7 +3,9 @@ import fastify from "fastify";
 export default () => {
   const app = fastify();
   // BEGIN (write your solution here)
-
+  app.get('/', (req, res) => {
+    res.send('Welcome to Fastify!');
+  })
   // END
   return app;
 };
