ad2644eba6deb77e43fc9cffb2a78f2a9ff1e5c2
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:42:23 +0300

    Update server.js

--- 5/src/server.js
+++ 5/src/server.js
@@ -9,7 +9,26 @@ export default async () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
+  await app.register(view, { engine: { pug } });
 
+  app.get("/users", (req, res) => {
+    const data = {
+      users,
+    };
+    res.view('src/views/users/index', data);
+  });
+  app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find(user => user.id === id);
+    if (!user) {
+      res.code(404).send('User not found');
+      return;
+    }
+    const data = {
+      user,
+    };
+    res.view('src/views/users/show', data);
+  })
   // END
 
   return app;
