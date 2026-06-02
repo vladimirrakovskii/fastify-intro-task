f29e3883a00aa49d2123c805ddacbd63a52bcd0f
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:24:05 +0300

    Update users.js

--- 14/src/routes/users.js
+++ 14/src/routes/users.js
@@ -8,6 +8,28 @@ export default (app) => {
   app.get("/users/new", (req, res) => res.view("src/views/users/new"));
 
   // BEGIN (write your solution here)
+  app.post("/users", (req, res) => {
+    const user = {
+      id: generateId(),
+      token: generateToken(),
+      firstName: req.body.firstName.trim(),
+      lastName: req.body.lastName.trim(),
+      email: req.body.email.trim().toLowerCase(),
+    };
 
+    users.push(user);
+    res.cookie("token", user.token);
+    res.redirect(`/users/${user.id}`);
+  });
+
+  app.get("/users/:id", (req, res) => {
+    const user = users.find(({ id }) => id.toString() === req.params.id);
+
+    if (!user || user.token !== req.cookies.token) {
+      return res.status(404).send("User not found");
+    }
+
+    return res.view("src/views/users/show", { user });
+  });
   // END
 };
