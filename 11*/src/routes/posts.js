af6ce8d92c89ea48c9379388b531ec66b6d548d5
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:17:03 +0300

    Update posts.js

--- 11*/src/routes/posts.js
+++ 11*/src/routes/posts.js
@@ -5,6 +5,39 @@ export default (app) => {
   const posts = generatePosts();
 
   // BEGIN (write your solution here)
+  app.get("/posts/:id", (req, res) => {
+    const post = posts.find((item) => item.id === req.params.id);
 
+    if (!post) {
+      res.status(404).send("Page not found");
+      return;
+    }
+
+    res.view("src/views/posts/show", { post });
+  });
+
+  app.get("/posts", (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const perPage = 5;
+    const totalPages = Math.ceil(posts.length / perPage);
+
+    let currentPage = Math.max(1, page);
+    if (currentPage > totalPages) {
+      currentPage = totalPages + 1;
+    }
+
+    const chunks = _.chunk(posts, perPage);
+    const currentPosts = chunks[currentPage - 1] || [];
+
+    res.view("src/views/posts/index", {
+      posts: currentPosts,
+      currentPage: currentPage,
+      totalPages: totalPages,
+      hasPrevious: currentPage > 1,
+      hasNext: currentPage < totalPages,
+      previousPage: currentPage - 1,
+      nextPage: currentPage + 1
+    });
+  });
   // END
 };
