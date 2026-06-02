8b115c1d6f074ede3aad825348c81e735eafb5b1
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:38:49 +0300

    Update server.js

--- 3/src/server.js
+++ 3/src/server.js
@@ -8,7 +8,13 @@ export default () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
-
+  app.get('/users', (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const per = parseInt(req.query.per) || 5;
+    let startIndex = (page - 1) * per;
+    let endIndex = startIndex + per;
+    res.send(users.slice(startIndex, endIndex));
+  });
   // END
 
   return app;
