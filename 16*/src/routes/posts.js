3063d7baeaad4c0f2865f0edd54a96074be45599
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:29:42 +0300

    Update posts.js

--- 16*/src/routes/posts.js
+++ 16*/src/routes/posts.js
@@ -16,6 +16,35 @@ export default (app) => {
   });
 
   // BEGIN (write your solution here)
+  app.get("/posts", (req, res) => {
+    const flash = req.session.flash;
+    delete req.session.flash;
+    res.view("src/views/posts/index", { posts, flash });
+  });
 
+  app.post("/posts", (req, res) => {
+    const { title, body } = req.body;
+
+    if (!title || title.length < 2) {
+      res.view("src/views/posts/new", {
+        flash: {
+          type: 'error',
+          message: 'Ошибка создания поста!'
+        }
+      });
+      return;
+    }
+
+    const id = generateId();
+    const post = { title, body, id };
+    posts.push(post);
+
+    req.session.flash = {
+      type: 'success',
+      message: 'Пост был успешно создан!'
+    };
+
+    res.redirect('/posts');
+  });
   // END
 };
