45c54e9a2ce901798eabdafa00003cc81ac04636
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:32:55 +0300

    Update dbinit.js

--- 18*/src/dbinit.js
+++ 18*/src/dbinit.js
@@ -1,5 +1,14 @@
 export default (db) => {
   // BEGIN (write your solution here)
-  
+    db.serialize(() => {
+      db.run(`
+        CREATE TABLE products(
+            id INTEGER PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            price INTEGER NOT NULL
+        );
+      `);
+  })
   // END
 };
