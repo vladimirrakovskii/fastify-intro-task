d5a4ca659bd554b501ad2e901c894a1f0ba30189
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:37:55 +0300

    Update server.js

--- 2/src/server.js
+++ 2/src/server.js
@@ -9,7 +9,12 @@ export default () => {
   const app = fastify();
 
   // BEGIN (write your solution here)
-
+  app.get('/phones', (req, res) => {
+    res.send(data.phones);
+  });
+  app.get('/domains', (req, res) => {
+    res.send(data.domains);
+  });
   // END
 
   return app;
