e4adfad3b986ab6a16a86876bd50b7eb13256cf2
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:40:31 +0300

    Update server.js

--- 4/src/server.js
+++ 4/src/server.js
@@ -7,7 +7,16 @@ export default () => {
   const companies = getCompanies();
 
   // BEGIN (write your solution here)
-
+  app.get('/companies/:id', (req, res) => {
+    const { id } = req.params;
+    const company = companies.find((company) => company.id === id);
+    if (!company) {
+      res.code(404).send('Company not found');
+    }
+    else {
+      res.send(company);
+    }
+  });
   // END
 
   return app;
