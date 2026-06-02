1fc16d99f23bf065dfadabd4e3ab2660eb4e034b
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:32:26 +0300

    Update products.js

--- 18*/src/routes/products.js
+++ 18*/src/routes/products.js
@@ -4,6 +4,39 @@ export default (app, db) => {
   });
 
   // BEGIN (write your solution here)
-
+  app.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    db.get(`SELECT * from products WHERE id = ${id}`, (err, data) => {
+      const templateData = {
+        product: data,
+        err,
+      };
+      res.view("src/views/products/show", templateData);
+    });
+  });
+  app.get("/products", (req, res) => {
+    db.all(`SELECT * from products`, (err, data) => {
+      const templateData = {
+        products: data,
+        err
+      };
+      res.view("src/views/products/index", templateData);
+    });
+  });
+  app.post("/products", (req, res) => {
+    const {title, description, price} = req.body;
+    const stmt = db.prepare('INSERT INTO products (title, description, price) VALUES (?, ?, ?)');
+    stmt.run([title, description, price], function (err) {
+        if (err) {
+          const templateData = {
+            err,
+            product: {title, description, price},
+          };
+          res.view("src/views/products/show", templateData);
+          return;
+        }
+        res.redirect("/products");
+    })
+  })
   // END
 };
