89882e916ae982bc921217aaa1d5319e8b421cc0
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:21:59 +0300

    Update server.js

--- 13/src/server.js
+++ 13/src/server.js
@@ -12,7 +12,11 @@ export default async () => {
   await app.register(formbody);
 
   // BEGIN (write your solution here)
-
+  await app.register(middie);
+  app.use((req, res, next) => {
+    res.setHeader("Cache-Control", "no-store");
+    next();
+  });
   // END
 
   app.get("/", (req, res) => res.view("src/views/index"));
