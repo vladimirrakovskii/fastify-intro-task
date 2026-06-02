b2ef4aa8f26c84e6c454fa9df25f83b13f3f788c
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 12:56:06 +0300

    Update server.js

--- 7*/src/server.js
+++ 7*/src/server.js
@@ -13,7 +13,23 @@ export default async () => {
   app.get("/", (req, res) => res.view("src/views/index"));
 
   // BEGIN (write your solution here)
+ app.get("/users", (req, res) => {
+    const { term } = req.query;
+    let filteredUsers = users;
 
+    if (term) {
+      filteredUsers = users.filter((user) =>
+          user.username.toLowerCase().includes(term.toLowerCase())
+      );
+    }
+
+    const data = {
+      usersTerm: filteredUsers,
+      term: term || ''
+    };
+
+    return res.view("src/views/users/index", data);
+  });
   // END
 
   app.get("/users/:id", (req, res) => {
